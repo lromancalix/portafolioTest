6f7b6d6bfb389423a16927ba796edf7d8975826a
calix <roman.calix.net@gmail.com>
2020-07-19 21:18:05 -0500

    configuración de las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ItemComponent } from './pages/item/item.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'portafolio',
+    component: PortafolioComponent
+  },
+  {
+    path: 'item/:id',
+    component: ItemComponent
+  },
+  {
+    path: 'home',
+    component: PortafolioComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
